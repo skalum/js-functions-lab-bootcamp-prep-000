a784ddaba12d43b3c536ebe5911105f5a566fd52
skalum <stkalum@gmail.com>
2017-09-27 21:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown (days, holidays) {
+function holidayCountdown (days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
